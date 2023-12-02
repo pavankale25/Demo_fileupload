7ae22d2f0c8bae206811c2b4c64cb35a8db286fa
pavan <pavankale01998@gmail.com>
2023-12-02 16:03:15 +0530

    second time commit

--- src/Demo.java
+++ src/Demo.java
@@ -4,10 +4,18 @@ public class Demo {
 	public static void main(String [] args) {
 		System.out.println("Hello IPS");
 		Demo.a();
+		Demo d = new Demo();
+		d.nonStaticMethod();
 	}
 	
 	public static void a() {
+		System.out.println("This is static method");
+		System.out.println("Practice for static method");
+	}
+	
+	public void nonStaticMethod() {
 		System.out.println("This is non static method");
+		System.out.println("This method related to non static");
 	}
 
 }
