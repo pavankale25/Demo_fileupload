c0534b451bcc51f8dddf495b78527d5dafeb8980
pavan <pavankale01998@gmail.com>
2023-12-02 13:17:46 +0530

    Your commit message here

--- src/Demo.java
+++ src/Demo.java
@@ -2,10 +2,12 @@
 public class Demo {
 	
 	public static void main(String [] args) {
-		
-		
 		System.out.println("Hello IPS");
-		
+		Demo.a();
+	}
+	
+	public static void a() {
+		System.out.println("This is non static method");
 	}
 
 }
